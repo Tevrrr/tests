30e72175a607cbc02e1a94d8c95f67ea2b070b25
Tevrrr <piskynov.vlad@gmail.com>
2024-05-26 14:45:31 +0300

    add vite

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth/Auth';
 import Products from './pages/Products/Products';
@@ -6,15 +5,15 @@ import Algorithms from './pages/Alghoritms/Alghoritms';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/algorithms" element={<Algorithms />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Auth />} />
+				<Route path='/products' element={<Products />} />
+				<Route path='/algorithms' element={<Algorithms />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
