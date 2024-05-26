312dfedb08f26ab7f660cccf60022cb734672f4a
Tevrrr <piskynov.vlad@gmail.com>
2024-05-26 15:32:15 +0300

    fix all

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth/Auth';
-import Products from './pages/Products/Products';
-import Algorithms from './pages/Alghoritms/Alghoritms';
 import './App.css';
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
 		<Router>
 			<Routes>
 				<Route path='/' element={<Auth />} />
-				<Route path='/products' element={<Products />} />
-				<Route path='/algorithms' element={<Algorithms />} />
 			</Routes>
 		</Router>
 	);
